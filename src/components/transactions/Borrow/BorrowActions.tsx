b58b50739dcadae467e21ce3242197376a599d00
Shawn <shawn@karboncard.com>
2022-11-29 09:28:30 +0800

    Add success page

--- src/components/transactions/Borrow/BorrowActions.tsx
+++ src/components/transactions/Borrow/BorrowActions.tsx
@@ -2,6 +2,8 @@ import { Trans } from '@lingui/macro';
 import { BoxProps } from '@mui/material';
 
 import { TxActionsWrapper } from '../TxActionsWrapper';
+import { useCallback } from "react";
+import { useModalContext } from "src/hooks/useModal";
 
 export interface BorrowActionsProps extends BoxProps {
   amountToBorrow: string;
@@ -13,18 +15,20 @@ export const BorrowActions = ({
   amountToBorrow,
   sx,
 }: BorrowActionsProps) => {
-  const mainTxState = {
-    txHash: '',
-    loading: false,
-    success: false
-  }
+  const { setMainTxState, mainTxState } =  useModalContext();
+  
+  const borrowAction = useCallback(async () => {
+    setMainTxState({ txHash: '', loading: true, success: false });
+    await new Promise(resolve => setTimeout(() => resolve(undefined), 2000));
+    setMainTxState({ txHash: '0x01', loading: false, success: true });
+  }, [setMainTxState]);
 
   return (
     <TxActionsWrapper
       mainTxState={mainTxState}
       requiresAmount={true}
       amount={amountToBorrow}
-      handleAction={async () => {}}
+      handleAction={borrowAction}
       actionText={<Trans>Borrow {symbol}</Trans>}
       actionInProgressText={<Trans>Borrowing {symbol}</Trans>}
       preparingTransactions={false}
--- src/components/transactions/TxActionsWrapper.tsx
+++ src/components/transactions/TxActionsWrapper.tsx
@@ -1,6 +1,5 @@
 import { Trans } from '@lingui/macro';
 import { Box, BoxProps, Button, CircularProgress } from '@mui/material';
-import isEmpty from 'lodash/isEmpty';
 import { ReactNode } from 'react';
 import { TxStateType } from 'src/hooks/useModal';
 
@@ -31,7 +30,7 @@ export const TxActionsWrapper = ({
   
   function getMainParams() {
     if (isAmountMissing) return { disabled: true, content: <Trans>Enter an amount</Trans> };
-    if (preparingTransactions || isEmpty(mainTxState)) return { disabled: true, loading: true };
+    if (preparingTransactions) return { disabled: true, loading: true };
     if (mainTxState?.loading)
       return { loading: true, disabled: true, content: actionInProgressText };
     return { content: actionText, handleClick: handleAction };
